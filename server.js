07c5cf919487d3c8315627c7e056b183c01b134c
Glitch (hello-express) <none>
2020-05-19 13:42:03 +0000

    😻🛋 Checkpoint ./server.js:4291982/1297

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
- 
-const app = express();
- 
-app.use('/', createProxyMiddleware({ target: 'http://itch.io', changeOrigin: true }));
-app.listen(3000);
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({});
+
+// To modify the proxy connection before data is sent, you can listen
+// for the 'proxyReq' event. When the event is fired, you will receive
+// the following arguments:
+// (http.ClientRequest proxyReq, http.IncomingMessage req,
+//  http.ServerResponse res, Object options). This mechanism is useful when
+// you need to modify the proxy request before the proxy connection
+// is made to the target.
+//
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+});
+
+var server = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  proxy.web(req, res, {
+    target: 'http://www.itch.io'
+  });
+});
+
+console.log("listening on port 3000")
+server.listen(3000);
