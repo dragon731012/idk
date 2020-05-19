e872e9b327f39135a7b810dd32e5c0689a615a89
Glitch (hello-express) <none>
2020-05-19 13:18:06 +0000

    💼🐏 Checkpoint ./package.json:4291982/1287 ./server.js:4291982/1384

--- server.js
+++ server.js
@@ -1,34 +1,8 @@
-// server.js
-// where your node app starts
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const proxy = httpProxy.createProxyServer().listen(8000);;
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+http.createServer(function(req, res) {
+  proxy.web(req, res, { target: 'https://www.google.com/' });
 });
