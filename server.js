cc5443926bff55549f5085262c1b4d89d023bde0
Glitch (hello-express) <none>
2020-05-19 14:06:02 +0000

    ☃️🦀 Checkpoint ./server.js:4291982/130

--- server.js
+++ server.js
@@ -15,13 +15,14 @@ var proxy = httpProxy.createProxyServer({});
 // is made to the target.
 //
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('X-Frame-Options', 'yeet');
-  proxyReq.setHeader('Content-Security-Policy', 'frame-ancestors *');
+  res.setHeader('X-Frame-Options', 'yeet');
+  res.setHeader('Content-Security-Policy', 'frame-ancestors *');
 });
 
 var server = http.createServer(function(req, res) {
   // You can define here your custom logic to handle the request
   // and then proxy the request.
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   proxy.web(req, res, {
     target: 'http://www.itch.io'
   });
