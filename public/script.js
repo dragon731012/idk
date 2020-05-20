d745c342d65bb5d66a0ffd1a7bb10787dcc9b4f9
Glitch (virtual-browser) <none>
2020-05-20 17:24:19 +0000

    🎿🍨 Checkpoint ./public/index.html:4291982/217 ./public/script.js:4291982/281

--- public/script.js
+++ public/script.js
@@ -2,14 +2,42 @@ document.onload = function() {
   new BrowserWindow();
 };
 
-class BrowserWindow {
+class BrowserWindow extends PopupWindow {
   constructor(width=0, height=0) {
+    
+  }
+  
+  showBookmarks() {
+    
+  }
+  
+  restoreTabs() {
+    
+  }
+  
+  newTab() {
     
   }
 }
 
-class PopupWindow extends BrowserWindow {
-  constructor() {
+class PopupWindow {
+  constructor(width=0, height=0) {
     super(100, 100);
   }
+  
+  minimise() {
+    
+  }
+  
+  maximise() {
+    
+  }
+  
+  close() {
+    
+  }
+}
+
+class CustomWindow {
+  
 }
