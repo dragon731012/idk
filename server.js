1e10e86029b90afbc45ab408b27d5130ca6ed806
Glitch (virtual-browser) <none>
2020-05-20 01:17:40 +0000

    ☺️🌖 Checkpoint ./server.js:4291982/4208 ./public/index.html:4291982/60

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
+const express = require("express");
+const app = express();
 
-//
-// Create a proxy server with custom application logic
-//
-var proxy = httpProxy.createProxyServer({});
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
 
-// To modify the proxy connection before data is sent, you can listen
-// for the 'proxyReq' event. When the event is fired, you will receive
-// the following arguments:
-// (http.ClientRequest proxyReq, http.IncomingMessage req,
-//  http.ServerResponse res, Object options). This mechanism is useful when
-// you need to modify the proxy request before the proxy connection
-// is made to the target.
-//
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('X-Frame-Options', 'yeet');
-  proxyReq.setHeader('Content-Security-Policy', 'frame-ancestors *');
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  //response.redirect('/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-  //res.writeHead(200, { 'Content-Type': 'text/plain' });
-  proxy.web(req, res, {
-    target: 'http://www.itch.io',
-    changeOrigin: true
-  });
-});
 
-console.log("listening on port 3000")
-server.listen(3000);
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
