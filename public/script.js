bbc7c8cf1cef59bff578274c90bd2b937358132b
Glitch (virtual-browser) <none>
2020-05-21 17:32:40 +0000

    🎬🏢 Checkpoint ./public/script.js:4291982/767 ./public/CustomWindow.html:4291982/17

--- public/script.js
+++ public/script.js
@@ -2,6 +2,29 @@ document.onload = function() {
   new BrowserWindow();
 };
 
+class CustomWindow {
+  //static ready = fetch("/CustomWindow.html").then(d => d.text());
+  static templateURL = "/CustomWindow.html";
+  
+  constructor(width=0, height=0) {
+    this.width = width;
+    this.height = height;
+    this.loadTemplate();
+  }
+  
+  minimise() {
+    
+  }
+  
+  maximise() {
+    
+  }
+  
+  close() {
+    
+  }
+}
+
 class BrowserWindow extends CustomWindow {
   constructor(width=0, height=0) {
     
@@ -23,24 +46,5 @@ class BrowserWindow extends CustomWindow {
 class PopupWindow extends CustomWindow {
   constructor(width=0, height=0) {
     super(100, 100);
-  }
-}
-
-class CustomWindow {
-  constructor(width=0, height=0) {
-    this.width = width;
-    this.height = height;
-  }
-  
-  minimise() {
-    
-  }
-  
-  maximise() {
-    
-  }
-  
-  close() {
-    
   }
 }
