d4eca1695bb8c8d67871b73f2855a7b89ea045e0
Glitch (virtual-browser) <none>
2020-05-22 22:45:32 +0000

    🍵🏛 Checkpoint ./public/script.js:4291982/1676

--- public/script.js
+++ public/script.js
@@ -4,6 +4,47 @@ $(function() {
   myWin = new CustomWindow();
 });
 
+function makeDragable(elmnt, handle=undefined) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (handle != undefined) {
+    // if present, the header is where you move the DIV from:
+    handle.onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
 class CustomWindow {
   static tmeplate = fetch("/CustomWindow.html").then(d => d.text());
   
@@ -15,7 +56,9 @@ class CustomWindow {
       this.width = width;
       this.height = height;
       this.toggleSize();
-      this.name = $().text.bind(this.win.find('.appinfo').on('pointerdown', this.dragMouseDown));
+      var appinfo = this.win.find('.appinfo');
+      makeDragable(this.win[0], appinfo[0]);
+      this.name = $().text.bind(appinfo);
       this.name(name);
     })();
   }
