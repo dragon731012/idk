b81e2ba87a7f21ade03459fc0c020d4bd3d5ee59
Glitch (virtual-browser) <none>
2020-05-23 14:17:48 +0000

    👻🚃 Checkpoint ./server.js:4291982/567

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require("express");
+const sass = require("node-sass");
 const app = express();
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(function(req, res, next) {
   var url = req.url.split('.');
-  console.log(url[url.length-1])
-  express.static("public")(req, res, next);
+  var filename = req.url.split('/');
+  console.log(url[url.length-1]);
+  if (url[url.length-1] == 'css')
+    res.send(sass.renderSync({
+      file: __dirname + '/public/' + filename[filename.length-1].replace('css', 'sass')
+    }));
+  else
+    express.static("public")(req, res, next);
   next();
 });
 
-/* https://expressjs.com/en/starter/basic-routing.html
+// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   //response.redirect('/index.html');
-  response.sendFile(__dirname + "/views/index.html");
-});*/
+  response.sendFile(__dirname + "/public/index.html");
+});
 
 
 const listener = app.listen(process.env.PORT, () => {
