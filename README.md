b4812505c51b53a16ae93c71f87ce09a42f05141
Glitch (virtual-browser) <none>
2020-06-23 01:42:45 +0000

    👨‍❤️‍💋‍👨🚒 Checkpoint ./public/BrowserWindow.html:4291982/45 ./server.js:4291982/859 ./README.md:4291982/59 ./package.json:4291982/1352

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 Emulate a browser within your browser! (basically a proxy). Future additions may make this become more of a full web-os.
 
 ## Open-Source Libraries
-This project is made possible by the following open-source libraries:
-- [observable-slim](https://github.com/ElliotNB/observable-slim): [MIT lisence]()
+This project is made possible by the following open-source libraries (in no particular order):
+- [observable-slim](https://github.com/ElliotNB/observable-slim): [MIT lisence](https://github.com/ElliotNB/observable-slim/blob/master/LICENSE)
+- node-sass: __
+- Showdown: ___
 
 ### (c) 2020 Made by Ethan (GNU GENERAL PUBLIC LICENSE)
--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const sass = require("node-sass");
 const app = express();
+const fs = require('fs');
+const showdown  = require('showdown'),
+      converter = new showdown.Converter();
+
+app.get('/README.md', function(req, res, next) {
+  res.sendFile(__dirname + '/README.md');
+});
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.get('/*', function(req, res, next) {
   var url = req.url.split('.');
   var filename = req.url.split('/');
+  filename = filename[filename.length-1];
   console.log(url[url.length-1]);
-  if (url[url.length-1] == 'scss') {   
-    res.type('css');
-    res.write(sass.renderSync({
-      file: __dirname + '/public/' + filename[filename.length-1]
-    }).css);
-    res.end();
-  } else
-    express.static("public")(req, res, next);
+  switch (url[url.length-1]) {
+    case 'scss':
+      res.type('css');
+      res.write(sass.renderSync({
+        file: __dirname + '/public/' + filename
+      }).css);
+      res.end(); break;
+    case 'md':
+      res.send(converter.makeHtml(fs.readFileSync(__dirname + '/public/' + filename)));
+      break;
+    default:
+      express.static("public")(req, res, next);
+  }
 });
 
 // https://expressjs.com/en/starter/basic-routing.html
